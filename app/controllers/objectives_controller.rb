c3a52e2e2844c30d7a3abd8a34cdf94aecbe96b6
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-12 16:13:10 -0300

    ATOMIC COMMIT: new changes

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,40 +1,50 @@
 class ObjectivesController < ApplicationController
-    def index
-      @objectives = Objective.all
-    end
-  
-    def show 
-      @objective = Objective.find(params[:id])
-    end
-  
-    def new 
-      @objective = Objective.new
-    end
-  
-    def create
-      @objective = Objective.new(objective_params)
-      @objective.save ? redirect_to(@objective) : render(:new) 
-    end
-  
-    def edit
-      @objective = Objective.find(params[:id]) 
-    end
-  
-    def update 
-      @objective = Objective.find(params[:id])
-      @objective.update(objective_params) ? redirect_to(@objective) : render(:edit)
-    end
-  
-    def destroy
-      @objective = Objective.find(params[:id])
-      @objective.destroy
-      redirect_to objectives_path
-    end
-  
-    private 
-  
-    def objective_params
-      params.require(:objective).permit(:title, :description, :status, :image)
+  before_action :set_objective, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @objectives = Objective.all
+    @objective = Objective.new
+  end
+
+  def show
+  end
+
+  def new
+    @objective = Objective.new
+  end
+
+  def create
+    @objective = Objective.new(objective_params)
+    if @objective.save
+      redirect_to objectives_path, notice: 'Objetivo criado com sucesso.'
+    else
+      render :index
     end
   end
-  
+
+  def edit
+  end
+
+  def update
+    if @objective.update(objective_params)
+      redirect_to @objective, notice: 'Objetivo atualizado com sucesso.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @objective.destroy
+    redirect_to objectives_path, notice: 'Objetivo excluído com sucesso.'
+  end
+
+  private
+
+  def set_objective
+    @objective = Objective.find(params[:id])
+  end
+
+  def objective_params
+    params.require(:objective).permit(:title, :description, :status, :image)
+  end
+end
